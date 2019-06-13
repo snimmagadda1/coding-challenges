911b1069e68ae1f3f87fc8042f5485d11e8e1330
snimmag6 <sai.nimmagadda@optum.com>
2019-06-12 21:27:50 -0700

    This one is dumb b/c it is purely testing math

---
+++ src/Codility/PrefixSums/GenomicRangeQuery.java
@@ -0,0 +1,34 @@
+package Codility.PrefixSums;
+
+public class GenomicRangeQuery {
+
+    int solution(int A[], int N)
+    {
+        int i = 0;
+
+        if (N == 2){
+            return 0;
+        }
+
+        //initialize the current minimum average of the first two slots.
+        double minavg = (A[0] + A[1]) / 2.0;
+        int idx = 0;
+
+
+        for (i = 2; i < N; i++){
+            double tmp1 = (A[i - 1] + A[i]) / 2.0;
+            double tmp2 = (A[i - 2] + A[i - 1] + A[i]) / 3.0;
+
+            if (tmp1 < minavg){
+                idx = i - 1;
+                minavg = tmp1;
+            }
+            if (tmp2 < minavg){
+                idx = i - 2;
+                minavg = tmp2;
+            }
+        }
+        return idx;
+    }
+
+}
