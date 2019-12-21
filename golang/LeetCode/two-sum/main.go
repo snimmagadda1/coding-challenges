a18180aa9bb321e213d0c3e5df2c4df87b1de522
Sai Nimmagadda <sai.nimmagadda@duke.edu>
2019-12-21 15:16:12 -0600

    Add two sum golan

--- golang/LeetCode/two-sum/main.go
+++ golang/LeetCode/two-sum/main.go
@@ -1,7 +1,43 @@
+/*
+time complexity : O(n)
+space complexity: O(n)
+*/
+
 package main
 
 import "fmt"
 
+func twoSum(nums []int, target int) []int {
+
+	var toReturn []int
+
+	numberMap := make(map[int]int) // Key = number, value = number
+
+	for idx, val := range nums {
+		numberMap[val] = idx
+	}
+
+	fmt.Println("the numberMap is: ", numberMap)
+
+	for idx, value := range nums {
+
+		// Compliment
+		compliment := target - value
+
+		// If map has theh compliment and compliment is not the same number as value
+		if foundValue, ok := numberMap[compliment]; ok && numberMap[compliment] != idx {
+			toReturn = append(toReturn, idx, foundValue)
+			return toReturn
+		}
+	}
+
+	return toReturn
+}
+
 func main() {
-	fmt.Println("Hi")
+	// testInput := []int{2, 7, 11, 15}
+	testInput := []int{3, 2, 4}
+	testTarget := 6
+
+	fmt.Println(twoSum(testInput, testTarget))
 }
